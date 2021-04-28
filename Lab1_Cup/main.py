00cc565f7ee2c322c9575b2f3bc52732b22391f3
AlexXXll <55854752+AlexXXll@users.noreply.github.com>
2021-04-28 16:10:28 +0400

    Models B,P

--- Lab1_Cup/main.py
+++ Lab1_Cup/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 
 def main():
-    n = 10000
+    n = 1000
     d = int(input("Введите колличество траекторий: "))
     T = 1
     times = np.linspace(0., T, n)
@@ -11,11 +11,13 @@ def main():
     dB = np.sqrt(dt) * np.random.normal(size=(n-1, d))
     B0 = np.zeros(shape=(1, d))
     B = np.concatenate((B0, np.cumsum(dB, axis=0)), axis=0)
-    P = stats.poisson.rvs(mu = 0.9, loc = 0, size=(n-1,d))
+    mu = np.random.normal(size=(0, n))
+    distr = stats.poisson(mu=mu)
+    x = np.arange(d)
     plt.plot(times, B)
     plt.show()
     plt.figure()
-    plt.hist( P, T, density=True)
+    plt.vlines(x,0,distr.pmf(x))
     plt.show()
 
 
