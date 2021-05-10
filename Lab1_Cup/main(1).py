a63f693cc665415df74d7b2592ee42da3fc7d0c6
AlexXXll <>
2021-05-11 01:42:43 +0400

    release

--- Lab1_Cup/main(1).py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import scipy.stats as stats
-# лямбда = 0.9
-data = stats.poisson.rvs(mu = 0.9, loc = 0, size = 1000)
-plt.figure()
-plt.hist(data, T, density = True)
-plt.show()
--- Lab1_Cup/main.py
+++ Lab1_Cup/main.py
@@ -1,24 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+from scipy.stats import poisson
 
-def main():
-    n = 1000
-    d = int(input("Введите колличество траекторий: "))
-    T = 1
-    times = np.linspace(0., T, n)
-    dt = times[1] - times[0]
-    dB = np.sqrt(dt) * np.random.normal(size=(n-1, d))
-    B0 = np.zeros(shape=(1, d))
-    B = np.concatenate((B0, np.cumsum(dB, axis=0)), axis=0)
-    mu = np.random.normal(size=(0, n))
-    distr = stats.poisson(mu=mu)
-    x = np.arange(d)
-    plt.plot(times, B)
-    plt.show()
-    plt.figure()
-    plt.vlines(x,0,distr.pmf(x))
-    plt.show()
+class main():
+        n = 1000
+        Bd = int(input("Введите колличество траекторий Винеровского процесса: "))
+        Pd = int(input("Введите колличество траекторий Пуассоновского процесса: "))
+        T = 1
+        times = np.linspace(0., T, n)
+        dt = times[1] - times[0]
+
+        dB = np.sqrt(dt) * np.random.normal(size=(n-1, Bd))
+        B0 = np.zeros(shape=(1, Bd))
+        B = np.concatenate((B0, np.cumsum(dB, axis=0)), axis=0)
+
+        mu = 0.08
+        dP = poisson.rvs(mu, loc=0, size=(n-1, Pd), random_state=None)
+        P0 = np.zeros(shape=(1, Pd))
+        P = np.concatenate((P0, np.cumsum(dP, axis=0)), axis=0)
+
+        plt.figure(1)
+        plt.plot(times, B)
+        plt.figure(2)
+        plt.step(times, P)
+        plt.show()
 
 
 if __name__ == '__main__':
